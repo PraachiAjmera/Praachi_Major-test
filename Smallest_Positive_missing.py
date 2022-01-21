227bf5c5a134ff1eb7c00eb4301c7a8ef665df25
PraachiAjmera <56185000+PraachiAjmera@users.noreply.github.com>
2022-01-21 11:25:21 +0530

    first

---
+++ Smallest_Positive_missing.py
@@ -0,0 +1,28 @@
+arr_size = int(input("enter the array size:"))
+arr= int(input("enter the array elements:"))
+
+def segregate(arr, size):
+	j = 0
+	for i in range(size):
+		if (arr[i] <= 0):
+			arr[i], arr[j] = arr[j], arr[i]
+			j += 1
+	return j
+
+def findMissingPositive(arr, size):
+	for i in range(size):
+		if (abs(arr[i]) - 1 < size and arr[abs(arr[i]) - 1] > 0):
+			arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1]
+			
+	for i in range(size):
+		if (arr[i] > 0):
+			return i + 1
+	return size + 1
+
+def findMissing(arr, size):
+	shift = segregate(arr, size)
+	return findMissingPositive(arr[shift:], size - shift)
+    
+missing = findMissing(arr, arr_size)
+print("The smallest positive missing number is ", missing)
+
